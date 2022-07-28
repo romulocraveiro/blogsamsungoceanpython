abb99b4723a3ff7f206457800d92119fdabafadb
Romulo Craveiro <romulocraveiro@gmail.com>
2022-07-28 01:07:12 +0000

    db creation

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, redirect, url_for
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask ("hello") 
+app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# lista de dicionários:
-posts = [
-    {
-        "title": "O meu primeiro post",
-        "body": "Aqui é o texto do post",
-        "author": "Feulo",
-        "created": datetime(2022, 7, 25)
-    },
-     {
-        "title": "O meu segundo post",
-        "body": "Aqui é o texto do post",
-        "author": "Danilo",
-        "created": datetime(2022, 7, 26)
-    }
-]
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    # author = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) 
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) #index serve para localizar
+    email = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    posts = db.relationship('Post', backref='author')
+
+db.create_all()
 
 @app.route("/")
-def index(): #padrão de nomear esse tipo de função
-    return render_template("index.html", posts = posts) #vai ter uma variável posts no html 
-    #que recebe posts daqui
+def index():
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
+@app.route("/populate")
+def populate():
+    user = User(username='feulo', email='g@g.com', password_hash='a')
+    post1 = Post(title="Post 1", body = "Texto do Post", author=user)
+    post2 = Post(title="Post 2", body = "Texto do Post 2", author=user)
+    db.session.add(user)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    return redirect(url_for('index'))
+    
