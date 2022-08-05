8bf9e68414a2c9731a9d8ebf70098eba231d2b70
Romulo Craveiro <romulocraveiro@gmail.com>
2022-08-04 21:46:09 -0300

    rewriting app.py in index view - order of posts

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ db.create_all()
 
 @app.route("/")
 def index():
-    posts = Post.query.order_by(-Post.created).all()
+    posts = Post.query.order_by(Post.created.desc()).all()
     return render_template("index.html", posts=posts)
 
 @app.route("/register", methods = ["GET","POST"]) #mesma rota para get e post
